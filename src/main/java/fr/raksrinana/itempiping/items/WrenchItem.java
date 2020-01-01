cba0834fe074f8ab3f3f50a19b944fc3e5ee2221
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-01-01 20:46:33 +0100

    Update forge and mcp

--- src/main/java/fr/raksrinana/itempiping/items/WrenchItem.java
+++ src/main/java/fr/raksrinana/itempiping/items/WrenchItem.java
@@ -36,7 +36,7 @@ public class WrenchItem extends Item{
 				return result;
 			}
 		}
-		if(player.func_225608_bj_() && state.has(BlockStateProperties.HORIZONTAL_FACING)){
+		if(player.isCrouching() && state.has(BlockStateProperties.HORIZONTAL_FACING)){
 			BlockState state1 = cycleProperty(state, BlockStateProperties.HORIZONTAL_FACING);
 			world.setBlockState(pos, state1, 18);
 			return ActionResultType.SUCCESS;
